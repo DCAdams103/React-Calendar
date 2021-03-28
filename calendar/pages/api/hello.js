b2ca9714779a78b735a1f5869710b765eaff81f4
Dylan Adams <dylancadams1@gmail.com>
2021-03-28 11:04:47 -0400

    Successfully connected the website to Oracles NoSQL Cloud Database.
    The next step is to retrieve information from the database table given a certain email address, and present it on the page.

--- calendar/pages/api/hello.js
+++ calendar/pages/api/hello.js
@@ -1,5 +1,63 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { loadGetInitialProps } from 'next/dist/next-server/lib/utils';
+
+const NoSQLClient = require('oracle-nosqldb').NoSQLClient;
+let result;
+
+let client = new NoSQLClient({
+  auth: {
+      iam: {
+          configFile: './.oci/config',
+          profile: 'Default'
+      }
+  }
+});
+
+async function putRowsIntoCalendarTable(email) {
+  const tableName = 'calendar';
+  try {
+      // Put the data into the calendar table.
+      let result = await client.put(tableName, { id: 1, name: 'Dylan', email: email });
+      console.log('put: ' + result.success ? 'succeeded' : 'failed');
+
+  } catch(error) {
+      console.log(error)
+  }
+}
+
+async function getRowsFromCalendarTable({email}) {
+  const tableName = 'calendar';
+  try {
+    // Get the data from the calendar table with the given email
+      result = await client.get(tableName, { id: 1, email: email });
+      
+  } catch(error) {
+      console.log(error)
+  }
+
+  return result
+
+}
+
 export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+
+  // Get the email that was passed in
+  const { email } = req.query
+
+  try {
+
+    if(!email) { 
+      return res.status(400).json({message: 'email required'}) 
+    }
+
+  } catch(error)
+  {
+    console.log(error)
+  }
+
+  
+  // getRowsFromCalendarTable(email)
+
+  res.status(200).json({ name: getRowsFromCalendarTable(email) })
 }
--- calendar/pages/index.js
+++ calendar/pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+
+const aaa = async(email) => {
+  const res = await fetch(`http://localhost:3000/api/hello?email=${email}`)
+  const data = await res.json()
+  console.log(data.name)
+}
+
 export default function Home() {
+  aaa('testemail@gmail.com')
   return (
-    <div className={styles.container}>
+    <div>
       
     </div>
   )
